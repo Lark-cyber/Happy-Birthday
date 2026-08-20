6f1e1fe8bc4a4b77a1a65a7065235ff48314dd45
Lark-cyber <laprob49@gmail.com>
2026-08-20 15:28:18 +0000

    Retrait des chaines de caractère

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    GreetingImage(message = "Happy Birthday Sam", from = "From Emma")
+                    GreetingImage(message = stringResource(R.string.birthday_message), from = stringResource(R.string.from_text))
                 }
             }
         }
@@ -71,8 +72,8 @@ fun GreetingImage(message: String,from:String,modifier: Modifier= Modifier){
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         GreetingImage(
-            message = "Happy Birthday Sam!",
-            from = "From Emma"
+            message = stringResource(R.string.birthday_message),
+            from = stringResource(R.string.from_text)
         )
     }
 }
